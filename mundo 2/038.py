0592caf91c350bf4736498df55cd62dcdfdda93d
h1 <humbertoetiago@gmail.com>
2025-06-18 17:11:18 -0300

    Create 038.py

---
+++ mundo 2/038.py
@@ -0,0 +1,8 @@
+n1 = int(input("valor 1: "))
+n2 = int(input("valor 2: "))
+if n1 > n2:
+    print(f"VALOR 1 MAIOR: {n1}")
+elif n2 > n1:
+    print(f"VALOR 2 MAIOR: {n2}")
+else:
+    print(f"OS VALORE 1 E 2 SAO IGUAIS")
