c3be85c0a7be05a8362e9f207661db0644fa4295
h1 <humbertoetiago@gmail.com>
2025-07-22 09:23:50 -0300

    Update 057.py

--- mundo 2/057.py
+++ mundo 2/057.py
@@ -1,4 +1,5 @@
-a1 = "s"
-while a1 == "F" and "M":
-    a1 = str(input("SEXO F/M: ")).upper().strip()
-    
+a1 = str(input("DIGITE F/M: ")).upper()
+while a1 != "F" and a1 != "M":
+    print("COMANDO INVALIDO")
+    a1 = str(input("DIGITE F/M: ")).upper()
+print("1")
