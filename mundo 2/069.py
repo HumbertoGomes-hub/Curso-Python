3c01130daa027b36b8f3b3105420b7e4b10dce99
h1 <humbertoetiago@gmail.com>
2025-07-22 14:35:37 -0300

    Update 069.py

--- mundo 2/069.py
+++ mundo 2/069.py
@@ -4,10 +4,14 @@ homi = 0
 muie = 0
 while True:
     idade = int(input("IDADE: "))
-    sexo = str(input("SEXO: ")).upper().strip()
-    cont = str(input("QUER CONTINUAR S/N: ")).upper().strip()
-    if cont == "S" or cont == "S":
-        break
+    sexo = str(input("SEXO F/M: ")).upper().strip()
     if idade >= 18:
         maior +=1
-print(maior)
+    if sexo == "M":
+        homi += 1
+    if sexo == "F" and idade <= 20:
+        muie +=1
+    cont = str(input("QUER CONTINUAR S/N: ")).upper().strip()
+    if cont == "N":
+        break
+print(f"MAIORES DE 18: {maior}, HOMEMS: {homi}, MULHERES MENORES DE 20: {muie}")
