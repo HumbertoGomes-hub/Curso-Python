7a0f4170407ad2f314fafe48af2d66478cea92fb
h1 <humbertoetiago@gmail.com>
2025-07-22 09:45:15 -0300

    Update 058.py

--- mundo 2/058.py
+++ mundo 2/058.py
@@ -1,11 +1,16 @@
 import random
+import time
 print("ADIVINHE O NUMERO")
-pc = random.randint(1,5)
+pc = random.randint(1,10)
 n = int(input("DIGITE: "))
 tent = 1
+print(".....")
+time.sleep(3)
 while n != pc:
     print("ERROU")
     n = int(input("TENTE NOVAMENTE: "))
+    print("...")
+    time.sleep(3)
     tent +=1
 print("GANHOU")
 print("VOCE TENTOU {}".format(tent))
