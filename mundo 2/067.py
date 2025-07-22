20bfba87e8043f095e0f2deacc2290f84d4f6f55
h1 <humbertoetiago@gmail.com>
2025-07-22 14:06:58 -0300

    rty

---
+++ mundo 2/067.py
@@ -0,0 +1,10 @@
+print("TABUADA")
+while True:
+    tabu = int(input("DIGITE: "))
+    c = 0
+    if c <= 10:
+        res = tabu * c
+        c += 1
+        print(res)
+    if tabu== 999:
+        break
