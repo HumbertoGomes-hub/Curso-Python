2d85c6bfb2426d2fa2d014018b6c22868b509126
h1 <humbertoetiago@gmail.com>
2025-07-24 15:06:24 -0300

    Update teste2.py

--- mundo 2/teste2.py
+++ mundo 2/teste2.py
@@ -1,2 +1,10 @@
-print("Hello World!")
-print("Ola Mundo")
+lost = ['um','dois','tres','quatro']
+lost.insert(1,'hello')
+lost.remove('hello')
+lost.append('santos')
+lost.insert(3,'jatoba')
+del lost[3]
+lost.sort()
+print(len(lost))
+print(lost)
+lost.sort(reverse=True)
