c25860f65fca097cdc12b60087ab7a4cc34ab212
h1 <humbertoetiago@gmail.com>
2025-07-28 11:40:01 -0300

    Update 085.py

--- mundo 2/085.py
+++ mundo 2/085.py
@@ -1,16 +1,16 @@
 lmae = list()
 par = list()
 impar = list()
+a = 0
 for c in range (0,7):
-    lmae.append(int(input("DIGITE UM VALOR: ")))
-    for p in lmae:
-        if p%2 == 0:
-            par.append(lmae[:])
-        else:
-            impar.append(lmae[:])
-    lmae.clear()
-par.sort()
+    a = int(input("DIGITE UM VALOR: "))
+    if a%2 == 0:
+        par.append(a)
+    else:
+        impar.append(a)
 impar.sort()
-            
-print(f"PAR {par}, IMPAR {impar}")
+par.sort()
+lmae.append(impar[:])
+lmae.append(par[:])
+print(lmae)
         
