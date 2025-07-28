877f07d7f8ce55a42ad4bfa60b1675e2481dc201
h1 <humbertoetiago@gmail.com>
2025-07-28 10:58:57 -0300

    Update 084.py

--- mundo 2/084.py
+++ mundo 2/084.py
@@ -1,21 +1,33 @@
 nome = list()
 cadastro = list()
-maxp = list()
-minp = list()
+maxp = 0
+minp = 0
 while True:
-    nome.append(str(input("MOME: ").capitalize().strip()))
+    nome.append(str(input("NOME: ").capitalize().strip()))
     nome.append(float(input("PESO: ")))
-    cadastro.append(nome[:])
     if len(cadastro) == 0:
-        maxp = minp = cadastro
+        maxp = minp = nome[1]
     else:
-        if cadastro[1] > maxp:
-            maxp.append(cadastro[:])
+        if nome[1] > maxp:
+            maxp = nome[1]
+        if nome[1] < minp:
+            minp = nome[1]
+    cadastro.append(nome[:])
     trava = str(input("QUER CONTINUAR S/N: ").strip().upper())
     if trava[0] == "N":
         break
     nome.clear()
-print(f"cadas{cadastro}, max{maxp}")
+print(f"QUANTIDADE DE PESSOA CADASTRADAS: {len(cadastro)}")
+print(f"MAIORES PESOS SÃO {maxp}KG DAS PESSOAS ",end="")
+for p in cadastro:
+    if p[1] == maxp:
+        print(f"[{p[0]}]",end="")
+print()
+print(f"MENORES PESOS SÃO {minp}KG DAS PESSOAS",end="")
+for p in cadastro:
+    if p[1] == minp:
+        print(f"[{p[0]}]",end="")
+
     
     
     
