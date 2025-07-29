0003b7fe59284a8b24d92a23e8c8e3200acbbcba
h1 <humbertoetiago@gmail.com>
2025-07-29 16:24:46 -0300

    Create 089.py

---
+++ mundo 2/089.py
@@ -0,0 +1,40 @@
+from copy import deepcopy
+lista_alunos = list()
+aluno = list()
+notas_aluno = list()
+media = list()
+cont = 0
+while True:
+    aluno.append(str(input("NOME: ")).capitalize().strip())
+    nota1 = float(input("NOTA 1: "))
+    nota2 = float(input("NOTA 2: "))
+    media = [(nota1 + nota2)/2]
+    notas_aluno = [nota1,nota2]
+    aluno.append(deepcopy(notas_aluno))
+    aluno.append(deepcopy(media)) 
+    lista_alunos.append(deepcopy(aluno))
+    aluno.clear()
+    notas_aluno.clear()
+    media.clear
+    controle = str(input("QUER CONTINUAR [S/N]: ")).upper().strip()
+    cont +=1
+    if controle[0] == "N":
+        break
+    
+    
+for i,c in enumerate(range(cont)):
+    print("-="*30)
+    print(f"NUMERO: {i}  ALUNO: {lista_alunos[c][0]}   MEDIA: {lista_alunos[0][2][0]}")
+    print("-="*30)
+
+while True:
+    num = int(input("NUMERO DO ALUNO QUE DESEJA VER AS NOTAS: "))
+    print(f"ALUNO: {lista_alunos[num][0]} NOTA 1: {lista_alunos[num][1][0]} NOTA 2: {lista_alunos[num][1][1]}")
+    
+    
+    controle2 = str(input("QUER CONTINUAR [S/N]: ")).upper().strip()
+    cont +=1
+    if controle2[0] == "N":
+        break  
+    
+    
