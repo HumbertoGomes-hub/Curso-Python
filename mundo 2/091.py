f977fef15c5b6c32f66798681755cee7caa4fd17
h1 <humbertoetiago@gmail.com>
2025-08-02 19:12:43 -0300

    Update 091.py

--- mundo 2/091.py
+++ mundo 2/091.py
@@ -1,10 +1,23 @@
 from random import randint
+from operator import itemgetter
+from time import sleep
 dado = dict()
+rank = dict()
 for c in range(1,5):
     lado = randint(1,6)
     dado[f"jogador{c}"] = lado
 for k,v in dado.items():
-    print(f"O {k} tirou {v}")
+    print(f"O {k} tirou {v} no dado.")
+    sleep(0.4)
+rank = sorted(dado.items(), key=itemgetter(1), reverse=True)
+print("-="*27)
+for i,v in enumerate(rank):
+    print(f"O {i+1}° Lugar foi {v[0]} com o lado {v[1]}.")
+    sleep(0.4)
+
+
+
+
 
 
 
