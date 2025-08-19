6f9f2bc605fb98a9b86725ddbf63ceca62314837
h1 <humbertoetiago@gmail.com>
2025-08-19 19:19:02 -0300

    Update 092.py

--- mundo 2/092.py
+++ mundo 2/092.py
@@ -1,10 +1,19 @@
 import time
+from datetime import date
 cad = dict()
 nome1 = str(input("NOME: ")).capitalize().strip()
 nasc1 = int(input("ANO NASCIMENTO: "))
 ctps1 = int(input("NUMERO DA CARTEIRA DE TRABALHO (0 NÃO TEM): "))
-if ctps1 == 0:
+ano = date.today().year
+idade = ano - nasc1
+if ctps1 != 0:
     cad["nome"] = nome1
+    cad["ano_contrat"] = int(input("ANO DA CONTRATAÇÃO: "))
+    cad["salario"] = float(input("SALÁRIO: "))
+    cad["idade"] = ano
+    
+print(cad)  
+    
     
     
 
