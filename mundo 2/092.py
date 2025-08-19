e5ffa24bfe5aa7c86558da16f63e721f9b96d83b
h1 <humbertoetiago@gmail.com>
2025-08-19 19:47:47 -0300

    Update 092.py

--- mundo 2/092.py
+++ mundo 2/092.py
@@ -6,13 +6,28 @@ nasc1 = int(input("ANO NASCIMENTO: "))
 ctps1 = int(input("NUMERO DA CARTEIRA DE TRABALHO (0 NÃO TEM): "))
 ano = date.today().year
 idade = ano - nasc1
+apo = 0
 if ctps1 != 0:
     cad["nome"] = nome1
     cad["ano_contrat"] = int(input("ANO DA CONTRATAÇÃO: "))
     cad["salario"] = float(input("SALÁRIO: "))
-    cad["idade"] = ano
+    cad["idade"] = idade
+    cad["ctps"] = ctps1
+    apo = 65 - cad["idade"]
+    cad["idade_apo"] = apo + idade
+    print("-="*27)
+    print(f"Nome: {cad['nome']} | Idade: {cad['idade']}")
+    print("-"*27)
+    print(f"Numero ctps: {cad['ctps']} | Idade da aposentadoria: {cad['idade_apo']}")
+    print("-"*27)
+    print(f"Salario: {cad['salario']} | Ano contratação: {cad['ano_contrat']}")
+else:   
+    print("NUMERO CTPS INVALIDO!")
     
-print(cad)  
+
+    
+    
+
     
     
     
